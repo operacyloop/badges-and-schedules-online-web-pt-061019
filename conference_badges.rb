0b8cea30f1cd975c20d4e3dee7515ee43b0b50e1
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-08-10 05:36:01 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+# method that takes an argument, a name, and spits out a string
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(list)
+ list.collect { | name | badge_maker ( name )   }
+end
+
+def assign_rooms(list)
+ list.each_with_index.map {|sp, idx| "Hello, #{sp}! You'll be assigned to room #{idx+1}!"}
+end
+
+def printer(name)
+  batch_badge_creator(name).each do |badge|
+    puts badge
+  end
+  assign_rooms(name).each do |ass|
+    puts ass
+  end
+ end
